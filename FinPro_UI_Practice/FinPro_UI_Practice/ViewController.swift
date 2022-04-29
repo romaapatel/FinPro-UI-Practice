58fd1c644253ec040dd4efc476833e8119dd183c
Roma Patel <roma.p@simformsolutions.com>
2022-04-29 09:22:50 +0530

    initial Commit

---
+++ FinPro_UI_Practice/FinPro_UI_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinPro_UI_Practice
+//
+//  Created by Roma Patel on 29/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
